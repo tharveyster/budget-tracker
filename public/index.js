25b464ce851edd101a2b7cf7d7b71ee4e37bf0cb
tharvey <tharveyster@gmail.com>
2021-09-28 11:43:42 -0400

    Added a db.js file and cleaned up code in the other files

--- public/index.js
+++ public/index.js
@@ -1,14 +1,11 @@
 let transactions = [];
 let myChart;
 
-fetch("/api/transaction")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
+fetch('/api/transaction')
+  .then((response) => response.json())
+  .then((data) => {
     // save db data on global variable
     transactions = data;
-
     populateTotal();
     populateTable();
     populateChart();
@@ -20,17 +17,17 @@ function populateTotal() {
     return total + parseInt(t.value);
   }, 0);
 
-  let totalEl = document.querySelector("#total");
+  let totalEl = document.querySelector('#total');
   totalEl.textContent = total;
 }
 
 function populateTable() {
-  let tbody = document.querySelector("#tbody");
-  tbody.innerHTML = "";
+  let tbody = document.querySelector('#tbody');
+  tbody.innerHTML = '';
 
-  transactions.forEach(transaction => {
+  transactions.forEach((transaction) => {
     // create and populate a table row
-    let tr = document.createElement("tr");
+    let tr = document.createElement('tr');
     tr.innerHTML = `
       <td>${transaction.name}</td>
       <td>${transaction.value}</td>
@@ -46,13 +43,13 @@ function populateChart() {
   let sum = 0;
 
   // create date labels for chart
-  let labels = reversed.map(t => {
+  let labels = reversed.map((t) => {
     let date = new Date(t.date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   });
 
   // create incremental values for chart
-  let data = reversed.map(t => {
+  let data = reversed.map((t) => {
     sum += parseInt(t.value);
     return sum;
   });
@@ -62,41 +59,42 @@ function populateChart() {
     myChart.destroy();
   }
 
-  let ctx = document.getElementById("myChart").getContext("2d");
+  let ctx = document.getElementById('myChart').getContext('2d');
 
   myChart = new Chart(ctx, {
     type: 'line',
       data: {
         labels,
-        datasets: [{
-            label: "Total Over Time",
+        datasets: [
+          {
+            label: 'Total Over Time',
             fill: true,
-            backgroundColor: "#6666ff",
-            data
-        }]
-    }
+            backgroundColor: '#6666ff',
+            data,
+        },
+      ],
+    },
   });
 }
 
 function sendTransaction(isAdding) {
-  let nameEl = document.querySelector("#t-name");
-  let amountEl = document.querySelector("#t-amount");
-  let errorEl = document.querySelector(".form .error");
+  let nameEl = document.querySelector('#t-name');
+  let amountEl = document.querySelector('#t-amount');
+  let errorEl = document.querySelector('.form .error');
 
   // validate form
-  if (nameEl.value === "" || amountEl.value === "") {
-    errorEl.textContent = "Missing Information";
+  if (nameEl.value === '' || amountEl.value === '') {
+    errorEl.textContent = 'Missing Information';
     return;
-  }
-  else {
-    errorEl.textContent = "";
+  } else {
+    errorEl.textContent = '';
   }
 
   // create record
   let transaction = {
     name: nameEl.value,
     value: amountEl.value,
-    date: new Date().toISOString()
+    date: new Date().toISOString(),
   };
 
   // if subtracting funds, convert amount to negative number
@@ -113,41 +111,40 @@ function sendTransaction(isAdding) {
   populateTotal();
   
   // also send to server
-  fetch("/api/transaction", {
-    method: "POST",
+  fetch('/api/transaction', {
+    method: 'POST',
     body: JSON.stringify(transaction),
     headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    }
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
   })
-  .then(response => {    
-    return response.json();
-  })
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     if (data.errors) {
-      errorEl.textContent = "Missing Information";
-    }
-    else {
+      errorEl.textContent = 'Missing Information';
+    } else {
       // clear form
-      nameEl.value = "";
-      amountEl.value = "";
+      nameEl.value = '';
+      amountEl.value = '';
     }
   })
-  .catch(err => {
+  .catch((err) => {
     // fetch failed, so save in indexed db
     saveRecord(transaction);
 
     // clear form
-    nameEl.value = "";
-    amountEl.value = "";
+    nameEl.value = '';
+    amountEl.value = '';
   });
 }
 
-document.querySelector("#add-btn").onclick = function() {
+document.querySelector('#add-btn').addEventListener('click', function (event) {
+  event.preventDefault();
   sendTransaction(true);
-};
+});
 
-document.querySelector("#sub-btn").onclick = function() {
+document.querySelector('#sub-btn').addEventListener('click', function (event) {
+  event.preventDefault();
   sendTransaction(false);
-};
+});
